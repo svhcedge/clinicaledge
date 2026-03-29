19c00fcc504f975ef7c063a74930f82a2ca32012
svhcedge <svhcedge@gmail.com>
2026-03-29 00:34:44 -0400

    Android UI look update

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'clinicaledge-v1';
+const ASSETS = [
+  '/clinicaledge/',
+  '/clinicaledge/index.html',
+  'https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/fuse.js/7.0.0/fuse.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('/clinicaledge/'));
+    })
+  );
+});
