d95874461c3cc47b9a595ec66294576a30e4ccf4
svhcedge <svhcedge@gmail.com>
2026-04-01 22:43:18 -0400

    V5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you deploy — forces cache refresh
-const CACHE = 'clinicaledge-v1775006751';
+const CACHE = 'clinicaledge-v1775043359';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
